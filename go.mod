cfa1947c984d9b674437787c264ae9ea3c72f5cb
ecrupper <easton.crupper12@gmail.com>
2024-02-27 11:58:19 -0600

    main.go for running status check update

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/go-vela/gh-app-playground
+
+go 1.21
+
+require (
+	github.com/bradleyfalzon/ghinstallation/v2 v2.0.3
+	github.com/google/go-github/v59 v59.0.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-github/v39 v39.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v59/github"
+	"golang.org/x/oauth2"
+)
+
+const appID = 263
+
+func main() {
+	tr := http.DefaultTransport
+
+	appPrivateKeyFile := os.Getenv("APP_PRIVATE_KEY_FILE")
+	gitApiURL := os.Getenv("APP_GIT_API_URL")
+	targetOrg := os.Getenv("APP_TARGET_ORG")
+	targetRepo := os.Getenv("APP_TARGET_REPO")
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.NewAppsTransportKeyFromFile(tr, appID, appPrivateKeyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	itr.BaseURL = gitApiURL
+	client, err := github.NewClient(&http.Client{Transport: itr}).WithEnterpriseURLs(gitApiURL, gitApiURL)
+	if err != nil {
+		panic(err)
+	}
+
+	installID := int64(11602)
+
+	token, _, err := client.Apps.CreateInstallationToken(context.Background(), installID, &github.InstallationTokenOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token.GetToken()},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	secondClient, err := github.NewClient(tc).WithEnterpriseURLs(gitApiURL, gitApiURL)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new check suite
+	checkCreateOpts := github.CreateCheckRunOptions{
+		Name:    "vela-testing",
+		HeadSHA: "main",
+	}
+
+	content, _, err := secondClient.Checks.CreateCheckRun(context.Background(), targetOrg, targetRepo, checkCreateOpts)
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(10 * time.Second)
+
+	checkRunOpts := github.UpdateCheckRunOptions{
+		Name:       "vela-testing",
+		Status:     github.String("completed"),
+		Conclusion: github.String("success"),
+	}
+
+	contentResp, resp, err := secondClient.Checks.UpdateCheckRun(context.Background(), targetOrg, targetRepo, content.GetID(), checkRunOpts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(contentResp)
+	fmt.Println(resp)
+}
